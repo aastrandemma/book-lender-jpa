9e4db879de1360375202a287796531877868e579
Emma Åstrand <aastrand.emma@gmail.com>
2024-05-30 12:15:20 +0200

    Initial commit

---
+++ src/main/java/org/github/aastrandemma/booklenderjpa/Application.java
@@ -0,0 +1,13 @@
+package org.github.aastrandemma.booklenderjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLenderJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLenderJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/github/aastrandemma/booklenderjpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.aastrandemma.booklenderjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLenderJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
