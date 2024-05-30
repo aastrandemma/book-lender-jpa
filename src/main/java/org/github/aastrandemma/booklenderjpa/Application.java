1a8aaa14b3ec009f6e1eb7b62173a66a7341c741
Emma Åstrand <aastrand.emma@gmail.com>
2024-05-30 13:52:05 +0200

    feat: Add database configurations

--- src/main/java/org/github/aastrandemma/booklenderjpa/Application.java
+++ src/main/java/org/github/aastrandemma/booklenderjpa/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookLenderJpaApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(BookLenderJpaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
