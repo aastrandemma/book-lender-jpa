880b089950665cf3881c2e430e7c738edee38243
Emma Åstrand <aastrand.emma@gmail.com>
2024-06-05 11:54:31 +0200

    feat: Add entity for Author

---
+++ src/main/java/org/github/aastrandemma/booklenderjpa/entity/Author.java
@@ -0,0 +1,44 @@
+package org.github.aastrandemma.booklenderjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "authors_books",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id"))
+    private Set<Book> writtenBooks = new HashSet<Book>();
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void addBook(Book book) {
+        writtenBooks.add(book);
+    }
+
+    public void removeBook(Book book) {
+        writtenBooks.remove(book);
+    }
+}
